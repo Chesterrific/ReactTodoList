60b96f4b85c6940c6e6603f034a7428f5170ca45
Chester Guinto <chesterguinto@gmail.com>
2020-10-22 23:30:51 -0700

    Updates

---
+++ precache-manifest.befb4cdb16893756169a262335c2d2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eeb719dfdd5d6b52f0bb657e257b835",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "68c56649abe8ff640efb",
+    "url": "/ReactTodoList/static/css/main.b013d62c.chunk.css"
+  },
+  {
+    "revision": "25ec5ec0201b2991c562",
+    "url": "/ReactTodoList/static/js/2.b6c14caa.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/ReactTodoList/static/js/2.b6c14caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c56649abe8ff640efb",
+    "url": "/ReactTodoList/static/js/main.a50c6266.chunk.js"
+  },
+  {
+    "revision": "e290eee0718ada80d754",
+    "url": "/ReactTodoList/static/js/runtime-main.7891b788.js"
+  }
+]);
